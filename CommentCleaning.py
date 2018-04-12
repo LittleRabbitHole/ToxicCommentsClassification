c56d80892b331402ca5fb7c4899c84011e224a32
Angrrr_ <li.ang198744@gmail.com>
2018-04-12 09:32:07 -0400

    update

--- CommentCleaning.py
+++ CommentCleaning.py
@@ -5,21 +5,19 @@ Created on Wed Apr 11 11:21:25 2018
 
 @author: angli
 """
-
+import pandas as pd
 import os
 import logging
 import argparse
-import glob
-import copy
 import re
-import string
-import html
 from nltk.corpus import stopwords
 import nltk
 #nltk.download('popular')
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
-
+#import HTMLParser
+import html
+import pickle
 
 logging.basicConfig(level=logging.INFO)
 _log = logging.getLogger('get_CleanComments')
@@ -27,76 +25,92 @@ _log = logging.getLogger('get_CleanComments')
 
 #define some patterns
 URL_PATTERN=re.compile(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
-SMILEYS_PATTERN = re.compile(r"(?:X|:|;|=)(?:-)?(?:\)|\(|O|D|P|S){1,}", re.IGNORECASE)
+#SMILEYS_PATTERN = re.compile(r"(?:X|:|;|=)(?:-)?(?:\)|\(|O|D|P|S){1,}", re.IGNORECASE)
 NUMBERS_PATTERN = re.compile(r"(^|\s)(\-?\d+(?:\.\d)*|\d+)")
+IP_PATTERN = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
 
+wordnet_lemmatizer = WordNetLemmatizer()
 
-#input as training data   
-#output the ([list of IDs], [list of cleaned comments])
-#length should be same as input lines
-def get_cleanlines(tweettext):
-    #match out the tweetID following by ",": "12345678912345678",
-    tweetID_raw = re.findall(r'\"[0-9]{16,19}\",', tweettext)
-    #clean out the last ","
-    tweetID_lst = [x[:-1].replace(" ","") for x in tweetID_raw]
-    
-    #tweet text
-    tweetText_raw = re.split(r'\"[0-9]{16,19}\",', tweettext)[1::]
-    #clean out
-    tweetText_lst = [html.unescape(y.strip().replace('"','').replace('\n','')) for y in tweetText_raw]
-    
-    #check
-    if len(tweetID_lst) == len(tweetText_lst):
-        #return
-        return(len(tweetID_lst), tweetID_lst, tweetText_lst)
+#mapping nltk lemmatizer pos taggs
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
     else:
-        raise ValueError('Error processing files: lines not match')
+        return wordnet.NOUN
 
 
-
-#clean tweet text
-def clean_tweet(Nlines, tweetID_lst, tweetText_lst):
-    updated_cleantweet_lst = []
-    for n in range(Nlines):
-        if n%1000 ==0: print (n)
-        tweetID =  tweetID_lst[n]
-        #clean url, emoji, mention, smiley
-        cleantweet_Url = re.sub(URL_PATTERN, '', tweetText_lst[n])
-        cleantweet_UrlMention = re.sub(MENTION_PATTERN, '', cleantweet_Url)
-        cleantweet_UrlMentionRev = re.sub(RESERVED_WORDS_PATTERN, '', cleantweet_UrlMention)
-        cleantweet_UrlMentionRevEmoji = re.sub(EMOJIS_PATTERN, '', cleantweet_UrlMentionRev)
-        cleantweet_final = re.sub(SMILEYS_PATTERN, '', cleantweet_UrlMentionRevEmoji)
-        #lower case
-        tweetText = str(cleantweet_final).replace("N/A","").lower()
-        #into list
-        cleantweet_lst = tweetText.split(' ')
-        cleantweet_lst = list(filter(None, cleantweet_lst))
-        #if not none
-        if len(cleantweet_lst) >= 1 and cleantweet_lst[0] != '':
-            #remove ending hashtags
-            filtered_hashtags = remove_EndHashtags(cleantweet_lst)
-            #final remove leading/trailing puctuation each word
-            tweet_word_lst1 = [s.strip("`~()?:!.,;'""&*<=+ >#|-/{}%$^@[]") for s in filtered_hashtags]
-            #remove non-letter in middle
-            tweet_word_lst = [re.sub(r'[^a-zA-Z0-9]+', '', s) for s in tweet_word_lst1]
-            #remove stop words
-            filtered_words = [word for word in tweet_word_lst if word not in stopwords.words('english')]
-            #remove all non-letters
-            filtered_stopwords = [re.sub(r'^[^a-zA-Z]+$', '', s) for s in filtered_words]
-            #remove none
-            final_tweet_word_lst = list(filter(None, filtered_stopwords))
-            #rejoin into sentence
-            cleanedtweettext = ' '.join(final_tweet_word_lst)
-            #add tweetID
-            final_tweet_word_lst = [tweetID, cleanedtweettext]
-            #update cleantweet list
-            updated_cleantweet_lst.append(final_tweet_word_lst)
-        else:
-            #cleantweet_lst = [""]
-            #rejoin into sentence
-            cleanedtweettext = ' '.join(cleantweet_lst)
-            final_tweet_word_lst = [tweetID, cleanedtweettext]
-            updated_cleantweet_lst.append(final_tweet_word_lst)
+#clean comment text
+def cleanComment(comment):
+    #clean url, emoji, mention, smiley
+    clean_Url = re.sub(URL_PATTERN, '', comment)
+    clean_UrlSmile = clean_Url.replace(":-)" , " ").replace(":)"," ").replace(";-)", " ").replace(";)"," ") #':-)', ':)', ';-)', ';)'
+    clean_UrlSmileIP = re.sub(IP_PATTERN, '', clean_UrlSmile)
+    clean_final = re.sub(NUMBERS_PATTERN, '', clean_UrlSmileIP)
+    #lower case
+    commentText = str(clean_final).replace("\n"," ").lower()
+    #into list
+    comment_lst = commentText.split(" ")
+    comment_lst = list(filter(None, comment_lst))
+    #if not none
+    if len(comment_lst) >= 1 and comment_lst[0] != '':
+        #comment_lst = [HTMLParser.HTMLParser().unescape(y.strip().replace('"','').replace('\n','')) for y in comment_lst]
+        comment_lst = [html.unescape(y.strip().replace('"','').replace('\n','')) for y in comment_lst]
+        #final remove leading/trailing puctuation each word
+        comment_lst1 = [s.strip("`~()?:!.,;'""&*<=+ >#|-/{}%$^@[]") for s in comment_lst]
+        #remove non-letter in middle
+        comment_lst = [re.sub(r'[^a-zA-Z0-9]+', '', s) for s in comment_lst1]
+        #remove stop words
+        filtered_words = [word for word in comment_lst if word not in stopwords.words('english')]
+        #remove none
+        filtered_words = list(filter(None, filtered_words))  
+        #lemm
+        tagged = nltk.pos_tag(filtered_words)
+        lemed_filtered_words = [wordnet_lemmatizer.lemmatize(x[0], get_wordnet_pos(x[1])) for x in tagged] #journalists
+        #remove all non-letters
+        filtered_stopwords = [re.sub(r'^[^a-zA-Z]+$', '', s) for s in lemed_filtered_words]
+        #remove none
+        final_word_lst = list(filter(None, filtered_stopwords))  
+        #rejoin into sentence
+        cleanedtext = ' '.join(final_word_lst)
+    else:
+        #cleantweet_lst = [""]
+        #rejoin into sentence
+        cleanedtext = ' '.join(comment_lst)
     
     #return result  
-    return (updated_cleantweet_lst)
+    return (cleanedtext)
+
+
+
+if __name__ == "__main__":
+    #train = pd.read_csv("/Users/angli/Ang/OneDrive/Documents/Pitt_PhD/Class/2018Spring/ML/finalProject/data/train.csv")
+    #all_comments_lst = list(train["comment_text"])
+    #with open('/Users/angli/ANG/OneDrive/Documents/Pitt_PhD/Class/2018Spring/ML/finalProject/data/all_toxic_comments_lst.pkl', 'wb') as f:
+        #pickle.dump(all_comments_lst, f, protocol=2)
+
+    # read file    
+    comments_lst = pickle.load( open( "/home/angatpitt/wiki_comments/all_toxic_comments_lst.pkl", "rb" ) )
+    _log.info("Reading file ...")
+    
+    clean_comments = []
+    n=0
+    for comment in comments_lst:
+        n+=1
+        if n % 2000 == 0: 
+            _log.info("process line {}...".format(n))
+            
+        clean_comment = cleanComment(comment)
+        clean_comments.append(clean_comment)
+
+    _log.info("writing file ...")    
+    with open('/home/angatpitt/wiki_comments/clean_comments.pkl', 'wb') as f:
+        pickle.dump(clean_comments, f)
+    
+    _log.info("done")    
+
