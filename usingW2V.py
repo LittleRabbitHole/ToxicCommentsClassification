239650b89f78448da4dd82489536c7a8975a8bd9
Angrrr_ <li.ang198744@gmail.com>
2018-04-16 23:51:38 -0400

    update

--- usingW2V.py
+++ usingW2V.py
@@ -31,4 +31,19 @@ x = X[0:3000]
 x_tsne = tsne.fit_transform(x)
 
 df = pd.DataFrame(x_tsne, index=vocab, columns=['x', 'y'])
+df = df[0:200]
 
+import matplotlib.pyplot as plt
+df = pd.read_csv("/Users/Ang/Documents/GitHub/ToxicCommentsClassification/x_tsne.csv", index_col=0)
+df = df[100:200]
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+ax.scatter(df['x'], df['y'])
+
+for word, pos in df.iterrows():
+    ax.annotate(word, pos)
+    
+plt.show()
+fig.savefig('x_tsne.png')
