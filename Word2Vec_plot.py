b313d84650d95dcb675315e451e40c04c59f6f39
Angrrr_ <li.ang198744@gmail.com>
2018-04-17 23:26:58 -0400

    commit

--- Word2Vec_plot.py
+++ Word2Vec_plot.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 16 23:22:39 2018
+Created on Tue Apr 17 20:53:05 2018
 
 @author: Ang
 """
-import os
-import numpy as np
+
 import pandas as pd
+import numpy as np
 from gensim.models import Word2Vec
 from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import os
 
 model_dir_path = os.path.dirname(os.path.realpath("/home/ang/Comments/w2vmodel"))
 model_dir_path = os.path.dirname(os.path.realpath("/home/ang/Comments/w2vmodel"))
 model_path = '{}/w2vmodel'.format(model_dir_path)
 model = Word2Vec.load('{}/cleantxt_200.w2v'.format(model_path))
-print (model.most_similar('memory'))
-
 
 
 def tsne_plot(vocabs_df):
@@ -24,7 +24,7 @@ def tsne_plot(vocabs_df):
     labels = []
     tokens = []
 
-    for word, row in vocabs_df:
+    for word in model.vocab:
         tokens.append(model[word])
         labels.append(word)
     
@@ -37,7 +37,7 @@ def tsne_plot(vocabs_df):
         x.append(value[0])
         y.append(value[1])
         
-    plt.figure(figsize=(16, 16)) 
+    fig = plt.figure(figsize=(16, 16)) 
     for i in range(len(x)):
         plt.scatter(x[i],y[i])
         plt.annotate(labels[i],
@@ -46,42 +46,5 @@ def tsne_plot(vocabs_df):
                      textcoords='offset points',
                      ha='right',
                      va='bottom')
-    plt.show()
-    
-
-
-df = pd.read_csv("/Users/Ang/Documents/GitHub/ToxicCommentsClassification/x_tsne.csv", index_col=0)
-
-
-#all words
-vocab = model[model.vocab]
-
-vocab = list(model.vocab)
-X = model[vocab] #this is vector
-
-vocab = vocab[0:3000]
-#>>> len(X) 383628
-x = X[0:3000]
-
-x_tsne = tsne.fit_transform(x)
-
-df = pd.DataFrame(x_tsne, index=vocab, columns=['x', 'y'])
-df = df[0:200]
-
-import matplotlib.pyplot as plt
-df = pd.read_csv("/Users/Ang/Documents/GitHub/ToxicCommentsClassification/x_tsne.csv", index_col=0)
-df = df[100:200]
-
-
-
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-
-ax.scatter(df['x'], df['y'])
-
-for word, pos in df.iterrows():
-    ax.annotate(word, pos)
-    
-plt.show()
-fig.savefig('x_tsne.png')
+    #plt.show()
+    fig.savefig('/home/ang/Comments/x_tsne.png')
